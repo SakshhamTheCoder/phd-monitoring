4e9140540f7bd854d5fc6d8341c4286871f16423
Sakshham Bhagat <sakshhamtg@gmail.com>
2026-09-15 01:33:16 +0530

    chore(client): drop two dead imports

--- client-new/src/pages/presentations/PresentationListPage.jsx
+++ client-new/src/pages/presentations/PresentationListPage.jsx
@@ -7,7 +7,6 @@ import CustomButton from "../../components/forms/fields/CustomButton";
 import GridContainer from "../../components/forms/fields/GridContainer";
 import BulkSchedulePresentation from "../../components/forms/presentations/BulkSchedulePresentation";
 import SchedulePresentation from "../../components/forms/presentations/SchedulePresentation";
-import FormTable from "../../components/forms/formTable/FormTable";
 import FilterBar from "../../components/filterBar/FilterBar";
 import PagenationTable from "../../components/pagenationTable/PagenationTable";
 import SemesterStatsCard from "./SemsterStatsCard";
