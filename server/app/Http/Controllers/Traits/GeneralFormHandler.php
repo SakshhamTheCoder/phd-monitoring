22cb87da681d8385a1c2f8b5b4f532891bb0d1f2
Sakshham Bhagat <sakshhamtg@gmail.com>
2026-09-15 00:09:01 +0530

    fix(forms): compare array_search against false

--- server/app/Http/Controllers/Traits/GeneralFormHandler.php
+++ server/app/Http/Controllers/Traits/GeneralFormHandler.php
@@ -128,7 +128,7 @@ trait GeneralFormHandler
                 $student = $formInstance->student;
                 if ($student->checkDoctoralCommittee($user->faculty->faculty_code)) {
                     $index = array_search('doctoral', $formInstance->steps);
-                    if (!$index)
+                    if ($index === false)
                         $index = array_search('external', $formInstance->steps);
                     if ($index !== false && $index <= $formInstance->maximum_step)
                         return response()->json($formInstance->fullForm($user));
